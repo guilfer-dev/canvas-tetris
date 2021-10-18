b3833d83b7ef6a91ceb8998ea0b958c42916b0a4
Guilherme Fernandes <57098649+guilfer-dev@users.noreply.github.com>
2021-10-18 09:31:02 -0300

    [refactor] tetraminoes and its render
    Tetraminos now have all the rotation cells required in its matrix, also they now feature individual colors. Reggarding to the rendering method, now it doesnt rely on positio to be rendered.

--- script.js
+++ script.js
@@ -2,59 +2,85 @@ function draw() {
     const field = document.getElementById("field").getContext('2d');
 
     const I = {
-        0: [0, 0],
-        1: [40, 0],
-        2: [80, 0],
-        3: [120, 0]
+        color: 'red',
+        shape: [
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+
     }
+
     const O = {
-        0: [0, 0],
-        1: [0, 40],
-        2: [40, 0],
-        3: [40, 40]
+        color: 'orange',
+        shape: [
+            [1, 1],
+            [1, 1]
+        ]
     }
+
     const T = {
-        0: [0, 0],
-        1: [40, 0],
-        2: [80, 0],
-        3: [40, 40]
+        color: 'blue',
+        shape: [
+            [1, 1, 1],
+            [0, 1, 0],
+            [0, 0, 0]
+        ]
     }
+
     const J = {
-        0: [40, 0],
-        1: [40, 40],
-        2: [40, 80],
-        3: [0, 80]
+        color: 'green',
+        shape: [
+            [0, 1, 0],
+            [0, 1, 0],
+            [1, 1, 0]
+        ]
     }
+
     const L = {
-        0: [0, 0],
-        1: [0, 40],
-        2: [0, 80],
-        3: [40, 80]
+        color: 'grey',
+        shape: [
+            [1, 0, 0],
+            [1, 0, 0],
+            [1, 1, 0]
+        ]
     }
+
     const S = {
-        0: [40, 0],
-        1: [80, 0],
-        2: [0, 40],
-        3: [40, 40]
+        color: 'black',
+        shape: [
+            [0, 1, 1],
+            [1, 1, 0],
+            [0, 0, 0],
+        ]
     }
+
     const Z = {
-        0: [0, 0],
-        1: [40, 0],
-        2: [40, 40],
-        3: [80, 40]
+        color: 'purple',
+        shape: [
+            [1, 1, 0],
+            [0, 1, 1],
+            [0, 0, 0]
+        ]
     }
 
     const pieces = [I, O, T, J, L, S, Z]
 
-    function renderTetramino(piece, x, y) {
+    function renderTetramino(piece) {
 
-        field.fillStyle = 'red';
-        for (square in piece) {
-            field.fillRect(x + piece[square][0], y + piece[square][1], 40, 40)
+        field.fillStyle = piece.color;
+
+        for (let i = 0; i < piece.shape.length; i++) {
+            for (let j = 0; j < piece.shape[i].length; j++) {
+                if (piece.shape[j][i] === 1) {
+                    field.fillRect(i * 40, j * 40, 40, 40)
+                }
+            }
         }
     }
-    renderTetramino(pieces[Math.floor(Math.random() * 7)], 100, 200);
 
+    renderTetramino(pieces[Math.floor(Math.random() * 7)]);
 }
 
 draw();
